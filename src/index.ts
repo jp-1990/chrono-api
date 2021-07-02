1ca59d93884f3d7c3b90b44a71b52d1fbf59b110
James Plummer <plum_1990@hotmail.co.uk>
2021-07-02 19:45:25 +0100

    update imported recent changes [index]

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
-export {};
-import dotenv from "dotenv";
+import "./env";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import mongoose from "mongoose";
-import { typeDefs } from "./typeDefs";
-import { resolvers } from "./resolvers";
+import { merge } from "lodash";
 
-dotenv.config({ path: "./config.env" });
+import { Query, UserTypeDef, TaskTypeDef } from "./typeDefs";
+import { userResolvers, taskResolvers } from "./resolvers";
+import { setUserContext } from "./utils";
 
 async function startApolloServer() {
   const app = express();
   const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+    typeDefs: [Query, UserTypeDef, TaskTypeDef],
+    resolvers: merge(userResolvers, taskResolvers),
+    context: async ({ req }) => {
+      const user = await setUserContext(req);
+      return { user };
+    },
   });
   await server.start();
 
@@ -20,7 +24,6 @@ async function startApolloServer() {
 
   app.use((req, res) => {
     res.status(200);
-    res.send("Hello!");
     res.end();
   });
 
@@ -52,3 +55,7 @@ async function startApolloServer() {
   return { server, app };
 }
 startApolloServer();
+
+// {
+//   "authorization":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1ZjI1YTE3YjgxZmFkOTQ0MzA4MjBmMzgiLCJpYXQiOjE2MjUxNjI3OTEsImV4cCI6MTYzMjkzODc5MX0.vpKgq_awHYI1k0uQvdVNZO-1Cqc07f3lm2XTXT1T7ao"
+//   }
---
+++ src/utils/index.ts
@@ -0,0 +1,2 @@
+export * from "./create-send-jwt";
+export * from "./set-user-context";
